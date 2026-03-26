d46f710fe0f0e7cfe90d812a8d5e5d70a472ea7f
kenny <khaneesh50@gmail.com>
2026-03-26 17:19:38 +0530

    stuff

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App.tsx';
-import './index.css';
-import { HelmetProvider } from 'react-helmet-async';
-import { AuthProvider } from './contexts/AuthContext.tsx';
-import { Analytics } from '@vercel/analytics/react';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { HelmetProvider } from "react-helmet-async";
+import { AuthProvider } from "./contexts/AuthContext.tsx";
+import { Analytics } from "@vercel/analytics/next";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <HelmetProvider>
       <AuthProvider>
@@ -14,5 +14,5 @@ createRoot(document.getElementById('root')!).render(
         <Analytics />
       </AuthProvider>
     </HelmetProvider>
-  </StrictMode>
+  </StrictMode>,
 );
